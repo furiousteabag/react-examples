8bef75e56f24f4835befad2e48f62a5eab48a18d
Alexander Smirnov <furiousteabag@gmail.com>
2023-10-15 19:07:59 -0700

    add posts

---
+++ nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Post() {
+    return (
+        <>
+            <h1>First post!</h1>
+            <h2>
+                <Link href="/">Back to home</Link>
+            </h2>
+        </>
+
+    )
+}
